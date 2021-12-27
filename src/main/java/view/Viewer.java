140241b6ed3e6ebfd9154f76bba9e4a91f239187
Joana <up202006279@edu.fe.up.pt>
2021-12-27 17:20:11 +0000

    Fixed objects

--- src/main/java/view/Viewer.java
+++ src/main/java/view/Viewer.java
@@ -1,5 +1,54 @@
 package view;
 
-public abstract class Viewer {
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.swing.AWTTerminalFontConfiguration;
+import com.googlecode.lanterna.terminal.swing.SwingTerminalFontConfiguration;
+import model.Model;
 
+import java.awt.*;
+import java.io.IOException;
+
+public abstract class Viewer<T extends Model> {
+    protected T model;
+    private Screen screen;
+    protected TextGraphics graphics;
+
+    public Viewer(T model){
+        initScreen();
+        this.model=model;
+    }
+    public void initScreen() {
+        try {
+            Font font = new Font(Font.MONOSPACED, Font.PLAIN, 2);
+            AWTTerminalFontConfiguration cfg = new SwingTerminalFontConfiguration(true, AWTTerminalFontConfiguration.BoldMode.NOTHING, font);
+            Terminal terminal = new DefaultTerminalFactory()
+                    .setInitialTerminalSize(new TerminalSize(469, 350))
+                    .setTerminalEmulatorFontConfiguration(cfg)
+                    .createTerminal();
+
+            screen = new TerminalScreen(terminal);
+
+            screen.setCursorPosition(null);   // We don't need a cursor
+            screen.startScreen();             // Screens must be started
+            screen.doResizeIfNecessary();     // Resize screen if necessary
+            screen.clear();
+            graphics=screen.newTextGraphics();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public abstract void draw() throws IOException;
+
+    public void setGraphics(TextGraphics graphics) {
+        this.graphics = graphics;
+    }
 }
