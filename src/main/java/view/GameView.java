82ca1def8bb92baec1b2d27a39d96bd46def2b94
inesc <ines.caardoso@gmail.com>
2021-12-30 02:56:12 +0000

    Added the classes for ghost's view functions, no code yet

--- src/main/java/view/GameView.java
+++ src/main/java/view/GameView.java
@@ -3,8 +3,12 @@ package view.ElementsView;
 import com.googlecode.lanterna.screen.Screen;
 import model.Elements.Coins.PowerCoin;
 import model.Elements.Coins.SmallCoin;
+import model.Elements.Ghosts.Red;
 import model.Elements.Wall;
 import model.GameModel;
+import view.ElementsView.Coins.PowerCoinView;
+import view.ElementsView.Coins.SmallCoinView;
+import view.ElementsView.Ghosts.*;
 import view.Viewer;
 
 import java.io.IOException;
@@ -12,11 +16,19 @@ import java.io.IOException;
 public class GameView extends Viewer<GameModel> {
     private final PacmanView pacmanViewer;
     private final WallView[] wallsViewers;
+    private final RedView redViewer;
+    private final PinkView pinkViewer;
+    private final OrangeView orangeViewer;
+    private final CyanView cyanViewer;
 
     public GameView(GameModel gameModel) throws IOException {
         super(gameModel);
         pacmanViewer = new PacmanView(gameModel.getMap().getPacman(), graphics);
         wallsViewers = new WallView[gameModel.getMap().getWalls().size()];
+        redViewer = new RedView(gameModel.getMap().getRed(), graphics);
+        pinkViewer = new PinkView(gameModel.getMap().getPink(), graphics);
+        orangeViewer = new OrangeView(gameModel.getMap().getOrange(), graphics);
+        cyanViewer = new CyanView(gameModel.getMap().getCyan(), graphics);
 
         int i = 0;
         for( Wall wall : gameModel.getMap().getWalls())
@@ -37,7 +49,14 @@ public class GameView extends Viewer<GameModel> {
         for( SmallCoin smallCoin : getModel().getMap().getSmallCoins().values())
             new SmallCoinView(smallCoin, graphics).draw();
 
+
+        redViewer.draw();
+        pinkViewer.draw();
+        orangeViewer.draw();
+        cyanViewer.draw();
+
         pacmanViewer.draw();
+
 
         getScreen().refresh(Screen.RefreshType.AUTOMATIC);
 
