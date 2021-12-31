4d1f8a93da7888096ef6410056e7b107d1069f34
inesc <ines.caardoso@gmail.com>
2021-12-31 14:07:19 +0000

    Added tests related with the Ghost's movement

--- src/test/java/model/Elements/GhostsTest.java
+++ src/test/java/model/Elements/GhostsTest.java
@@ -1,15 +1,23 @@
 package model.Elements;
 
+import model.Elements.Ghosts.Ghost;
 import model.Maps.Builders.MapBuilder;
 import model.Maps.Builders.MapReader;
 import model.Maps.Map;
 import model.Position;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
 public class GhostsTest extends Assertions {
+    Ghost ghost;
+
+    @BeforeEach
+    public void createPacman(){
+        ghost = new Ghost(new Position(3*8+1, 3*12-1));
+    }
 
     // The following 4 tests, test if each ghost is being read correctly
     @Test
@@ -43,4 +51,14 @@ public class GhostsTest extends Assertions {
         // using a diferent map then the one used in the game, so is easier to test
         assertEquals(map.getCyan().getPosition(), new Position(2 * 8 + 1, 30 * 12 - 1));
     }
+
+    @Test
+    void changePositionTest(){
+        long deltatime = -8/62/3;
+        double velocity = 62/3;
+        Position pos1 = ghost.move(deltatime, Direction.Up);
+        Position pos2 = new Position((3*8+1)-(int)(velocity*deltatime/1000), 3*12-1);
+        Assertions.assertTrue(pos1.equals(pos2));
+    }
+
 }
