5b464e9d7a8c3eff18425776c8a3a404ae2d5fa4
inesc <ines.caardoso@gmail.com>
2022-01-15 10:43:22 +0000

    Created ScoreView class, no code yet

---
+++ src/main/java/g0902/view/ScoreView.java
@@ -0,0 +1,18 @@
+package g0902.view;
+
+import com.googlecode.lanterna.graphics.TextGraphics;
+import g0902.view.ElementsView.View;
+
+import java.io.IOException;
+
+public class ScoreView extends View {
+
+    public ScoreView(TextGraphics graphics) {
+        super(graphics);
+    }
+
+    @Override
+    public void draw() throws IOException {
+
+    }
+}
