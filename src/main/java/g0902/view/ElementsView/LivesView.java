19bb99642743269ebbc6af601f1f30f27e4c7380
inesc <ines.caardoso@gmail.com>
2022-01-16 00:23:07 +0000

    Created LivesView

---
+++ src/main/java/g0902/view/ElementsView/LivesView.java
@@ -0,0 +1,17 @@
+package g0902.view.ElementsView;
+
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+import java.io.IOException;
+
+public class LivesView extends  View{
+
+    public LivesView(TextGraphics graphics) {
+        super(graphics);
+    }
+
+    @Override
+    public void draw() throws IOException {
+
+    }
+}
