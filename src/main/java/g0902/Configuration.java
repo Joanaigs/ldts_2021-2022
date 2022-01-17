b4b1cad25eb9fc8bb03156c844308ce991610b82
inesc <ines.caardoso@gmail.com>
2022-01-17 01:10:05 +0000

    Implemented Configuration Code

--- src/main/java/g0902/Configuration.java
+++ src/main/java/g0902/Configuration.java
@@ -1,5 +1,28 @@
 package g0902;
 
 public class Configuration {
+    private static Configuration config;
+    long frightenedTime;
+    long chaseTime;
 
+    private Configuration(){
+        frightenedTime = 9000;  //ms
+        chaseTime = 20000;
+    }
+
+    public static Configuration getInstance() {
+        if(config==null)
+            config = new Configuration();
+        return config ;
+    }
+
+    public long getFrightenedTime() {return frightenedTime;}
+
+    public long getChaseTime() {return chaseTime;}
+
+    public void nextLevel(){
+        if( frightenedTime >1000)
+            frightenedTime -= 2000;
+        chaseTime += 2000;
+    }
 }
